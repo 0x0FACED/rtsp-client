a6f7d8dbbcaf71339bd4b12087e7554e5d318fc3
MisterGnida <https.genius@gmail.com>
2024-04-30 16:16:43 +0300

    Added button to stop stream

--- app/src/main/java/com/groft/rtspclient/ui/views/RtspStreamView.kt
+++ app/src/main/java/com/groft/rtspclient/ui/views/RtspStreamView.kt
@@ -5,15 +5,20 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.navigation.NavController
+import com.groft.rtspclient.R
+import com.pedro.library.rtsp.RtspCamera1
 import com.pedro.library.rtsp.RtspCamera2
 import com.pedro.library.view.OpenGlView
 
@@ -55,7 +60,9 @@ fun RtspStreamView(
         Button(
             onClick = { onStopStream() },
         ) {
-            Text("Stop Stream")
+            IconButton(onClick = { onStopStream() }) {
+                Icon(painter = painterResource(id = R.drawable.stop_button), contentDescription = "stop stream")
+            }
         }
     }
 
